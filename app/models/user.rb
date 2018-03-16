19d3139deb858b97b9406c7be50fa8ca7cc9b9fe
wencakisa <wencakisa@gmail.com>
2018-03-16 10:40:37 +0200

    Setup Devise gem

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 3, maximum: 20 }
+end
