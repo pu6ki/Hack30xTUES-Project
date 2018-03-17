338315f4d6a9b132896b18cc9948dfa082bdfaf9
wencakisa <wencakisa@gmail.com>
2018-03-17 14:36:12 +0200

    Added listing of technologies

---
+++ app/views/technologies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! 'technology', technology: @technology
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'contests#index'
+
+  devise_for :users, only: [:sessions]
 
   resources :recruiters
   resources :contestants
   resources :schools
+  resources :technologies
   resources :contests do
     resources :submissions
     resources :test_cases
   end
-
-  root to: 'contests#index'
 end
