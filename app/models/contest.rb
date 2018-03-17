40636ed50ef8a5572d0a814ffc56351e26a95112
wencakisa <wencakisa@gmail.com>
2018-03-17 15:50:56 +0200

    Added max_score helper method in contest

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -9,6 +9,10 @@ class Contest < ApplicationRecord
   has_many :test_cases
   has_many :submissions
 
+  def max_points
+    test_cases.count * TestCase::PASSED_TEST_CASE_POINTS
+  end
+
   def to_s
     "\"#{title}\" by #{recruiter}"
   end
