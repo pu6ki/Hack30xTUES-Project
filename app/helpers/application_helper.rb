2d54a663e577e32204dacdd0eb2b6df65de38ed1
matir8 <martin.markov28@gmail.com>
2018-05-13 22:39:56 +0300

    Markdown config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
   def markdown(text)
     options = {
-      filter_html:     true,
-      no_images:       true,
-      no_links:        true,
-      no_tags:         true,
-      no_styles:       true
+      hard_wrap: true,
+      space_after_headers: true,
+      fenced_code_blocks: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
