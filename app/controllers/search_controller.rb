bdef107c64b4bbf881186641558abcd179a521b0
wencakisa <wencakisa@gmail.com>
2018-05-16 17:37:25 +0300

    Fixed search results rendering on search#index template

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
     @contests = Contest.search search_params
     @schools = School.search search_params
     @recruiters = Recruiter.search search_params
+    @has_result = !(@contests.empty? && @schools.empty? && @recruiters.empty?)
   end
 
   def search_params
