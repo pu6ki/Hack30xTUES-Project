feeec48c0e717a21f235be98e619baea6106d09b
wencakisa <wencakisa@gmail.com>
2018-05-16 16:09:23 +0300

    Now permitting term from parameters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @contests = Contest.search(params[:term])
-    @schools = School.search(params[:term])
-    @recruiters = Recruiter.search(params[:term])
+    @contests = Contest.search search_params
+    @schools = School.search search_params
+    @recruiters = Recruiter.search search_params
+  end
+
+  def search_params
+    params.require(:term)
   end
 end
