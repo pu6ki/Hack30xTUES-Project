8989a8fb26f01c2dbd939d8e7b06de9b40b47571
wencakisa <wencakisa@gmail.com>
2018-05-17 09:48:30 +0300

    Moved has_result? into view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,6 @@ class SearchController < ApplicationController
     @contests = Contest.search search_params
     @schools = School.search search_params
     @recruiters = Recruiter.search search_params
-    @has_result = !(@contests.empty? && @schools.empty? && @recruiters.empty?)
   end
 
   def search_params
